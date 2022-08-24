950f4d359f8654b250207e86c9abfcd4f74b5258
MY5T3RI0 <66646109+MY5T3RI0@users.noreply.github.com>
2022-08-24 20:28:52 +0300

    Добавлены комментарии и проверка входных параметров.

--- Map/Model/Dict.cs
+++ Map/Model/Dict.cs
@@ -6,20 +6,53 @@ using System.Threading.Tasks;
 
 namespace Map.Model
 {
+    /// <summary>
+    /// Словарь.
+    /// </summary>
+    /// <typeparam name="TKey">Тип ключа.</typeparam>
+    /// <typeparam name="TValue">Тип элемента.</typeparam>
     class Dict<TKey, TValue>
     {
+        /// <summary>
+        /// Начальный размер массива элементов.
+        /// </summary>
         private int Size = 100;
+
+        /// <summary>
+        /// Массив элементов.
+        /// </summary>
         private Item<TKey, TValue>[] Items;
 
+        /// <summary>
+        /// Список ключей.
+        /// </summary>
         private List<TKey> keys = new List<TKey>();
 
+        /// <summary>
+        /// Создать новый словарь.
+        /// </summary>
         public Dict()
         {
             Items = new Item<TKey, TValue>[Size];
         }
 
+        /// <summary>
+        /// Добавить элемент в словарь.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <param name="value">Значение.</param>
         public void Add(TKey key, TValue value)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
+
+            if (value.Equals(default(TValue)))
+            {
+                throw new ArgumentNullException(nameof(value), "Значение не может быть нулевым");
+            }
+
             if (keys.Contains(key))
             {
                 throw new ArgumentException("Элемент с таким ключом уже существует", nameof(key));
@@ -50,8 +83,17 @@ namespace Map.Model
             return;
         }
 
+        /// <summary>
+        /// Удалить элемент.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
         public void Delete(TKey key)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
+
             if (!keys.Contains(key))
             {
                 return;
@@ -85,8 +127,18 @@ namespace Map.Model
             }
         }
 
+        /// <summary>
+        /// Поиск элемента в словаре.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <returns>Искомый элемент.</returns>
         public Item<TKey, TValue> Search(TKey key)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
+
             if (!keys.Contains(key))
             {
                 return default;
@@ -117,8 +169,18 @@ namespace Map.Model
             throw new Exception("Ключ найден, но элемента с таким ключом нет");
         }
 
+        /// <summary>
+        /// Хеширование ключа.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <returns>Хеш ключа.</returns>
         public int GetHash(TKey key)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
+
             return key.GetHashCode() % Items.Length;
         }
     }
--- Map/Model/Item.cs
+++ Map/Model/Item.cs
@@ -6,14 +6,40 @@ using System.Threading.Tasks;
 
 namespace Map.Model
 {
+    /// <summary>
+    /// Элемент хеш таблицы.
+    /// </summary>
+    /// <typeparam name="TKey">Тип ключа.</typeparam>
+    /// <typeparam name="TValue">Тип элемента.</typeparam>
     class Item<TKey, TValue>
     {
+        /// <summary>
+        /// Ключ.
+        /// </summary>
         public TKey Key { get; set; }
 
+        /// <summary>
+        /// Значение.
+        /// </summary>
         public TValue Value { get; set; }
 
+        /// <summary>
+        /// Создать новый ключ.
+        /// </summary>
+        /// <param name="key">Ключ.</param>
+        /// <param name="value">Значение.</param>
         public Item(TKey key, TValue value)
         {
+            if (key.Equals(default(TKey)))
+            {
+                throw new ArgumentNullException(nameof(key), "Ключ не может быть нулевым");
+            }
+
+            if (value.Equals(default(TValue)))
+            {
+                throw new ArgumentNullException(nameof(value), "Значение не может быть нулевым");
+            }
+
             Key = key;
             Value = value;
         }
